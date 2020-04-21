7ab94c3d23503741c277938cf758e517eba8695e
xiangming0305 <xiangming0305@yeah.net>
2020-04-21 09:16:38 +0700

    []

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Builder; 
 
 class AppServiceProvider extends ServiceProvider
 {
